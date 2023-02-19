c34fb73a361638201460b3317e5a4a4ce0781c32
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-02-19 23:05:28 +0100

    new

---
+++ Parsing/Parsing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Parsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int parsedIntFromString;
+            string inputFromUser;
+
+            string stringForFloat = "0,67"; // datatype should be float
+            string stringForInt = "12345"; // datatype should be int
+
+            float parsedFloat = float.Parse(stringForFloat);
+            int parsedInt = int.Parse(stringForInt);
+
+            Console.WriteLine("Testowa suma: {0}", (float)parsedInt + parsedFloat);
+
+            //Jeszcze nie pora na obsługę wyjątków
+            //Nie moja wina :c
+            Console.Write("Podaj liczbę całkowitą: ");
+            inputFromUser = Console.ReadLine();
+            parsedIntFromString = Int32.Parse(inputFromUser);
+            Console.WriteLine("Out: {0}", parsedIntFromString + 1337);
+
+            Console.ReadKey();
+
+        }
+    }
+}

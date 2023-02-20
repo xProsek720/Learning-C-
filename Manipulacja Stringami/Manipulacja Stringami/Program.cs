dea89b1ef892fcc83a11dd9f2cc356bc6394c543
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-02-20 17:36:52 +0100

    upload

---
+++ Manipulacja Stringami/Manipulacja Stringami/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Manipulacja_Stringami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int printCounter = 1;                
+            int wiek = 22;
+            string imie = "Filip";
+
+            Console.WriteLine("{0}.Konkatynacja Stringów: ", printCounter++);
+            Console.WriteLine("Witaj jestem " + imie + " i mam " + wiek + "lata.\n");
+            Console.ReadKey();
+
+            Console.WriteLine("{0}.Formatowanie Stringów: ", printCounter++);
+            Console.WriteLine("Witaj jestem {0} i mam {1}lata.\n", imie, wiek);
+            Console.ReadKey();
+            
+            //Chyba najlepsze wg mnie
+            Console.WriteLine("{0}.Interpolacja Stringów: ", printCounter++);
+            Console.WriteLine($"Witaj jestem {imie} i mam {wiek}lata.\n");
+            Console.ReadKey();
+
+            //Łatwo opisać jednym przymiotnikiem i jednym rzeczownikiem RAW STRING
+            Console.WriteLine("{0}.Dosłowna interpretacja Stringów: ", printCounter++);
+            Console.WriteLine(@"C:\Windows\System32> rm -r -fo .");             //Taaa WRAJTLAJNEM System32 będę usuwał XD
+            Thread.Sleep(42); Console.Write("\nDELETING ALL FOLDER AND FILES"); Thread.Sleep(42); Console.Write("."); Thread.Sleep(42); Console.Write("."); Thread.Sleep(42); Console.WriteLine("."); Thread.Sleep(500); Console.WriteLine("DONE!"); Console.Write(@"C:\Windows> "); Console.ReadLine();
+            Console.ReadKey();                                                  //To jest learning, a nie produkcja!
+                                                                                //
+        }
+    }
+}
---
+++ Metody Stringów/Metody Stringów/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Metody_Stringów
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char szukanaLitera;
+            string imie, nazwisko, pelnaNazwa, tekst;
+
+            Console.Write("Podaj tekst: ");
+            tekst = Console.ReadLine();
+            
+            Console.WriteLine($"Wpisany tekst wielkimi literami: {tekst.ToUpper()}");
+            Console.WriteLine($"Wpisany tekst małymi literami: {tekst.ToLower()}");
+            Console.WriteLine($"Wpisany tekst bez zbędnych spacji: {tekst.Trim()}");
+            Console.WriteLine($"Substring od indeksu 1: {tekst.Substring(1)}");
+
+            Console.Write("\nPodaj literkę którą chciałbyś wyszukac w tekście: ");
+            szukanaLitera = Console.ReadKey().KeyChar;
+            Console.WriteLine($"\nPozycja szukanej litery: {tekst.IndexOf(szukanaLitera)}");
+
+            imie = "Filip";
+            nazwisko = "W@";
+            pelnaNazwa = String.Concat(imie, " ", nazwisko);
+            Console.WriteLine($"\nPo połączeniu {imie}, ' ' i {nazwisko} otrzymamy {pelnaNazwa}");
+            Console.ReadKey()
+
+        }
+    }
+}

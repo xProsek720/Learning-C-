27f3df7a255b14dbc1f23b8c66813c4993443376
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-02-20 17:59:45 +0100

    upload

---
+++ Stałe/Stałe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stałe
+{
+    internal class Program
+    {
+        //Najlepiej stałe tworzyć poza metodą w klasie
+        //ale chyba nie jest to zasada.
+
+        //Jak nazwa mówi są niezmienne przez cały proces wykonywania programu
+        const double shortPI = 3.14159265359;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Skrócone PI to: {shortPI}");
+            Console.Write("Teraz specjalnie spróbujemy zmienić naszą stałą!");
+            //shortPI = Math.Sin(shortPI);                                          //Bez zakomentowania tej lini program nie skompiluje się poprawinie
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(" (FUNKCJA WYŁĄCZONA!)");        
+            Console.ResetColor();
+        }
+    }
+}

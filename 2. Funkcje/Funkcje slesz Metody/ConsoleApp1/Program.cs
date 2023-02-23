8b3134e53927430a3a73e24ad05c430a28c4ea14
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-02-24 00:48:05 +0100

    upload

---
+++ 2. Funkcje/Funkcje slesz Metody/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public int InputInt() 
+        {
+            Console.Write("Podaj liczbę: ");
+            string inputInt = Console.ReadLine();
+            int returnInt;
+            if (int.TryParse(inputInt, out returnInt))
+            { 
+                return returnInt;
+            }
+            else 
+            {
+                return 0;
+            }
+        }
+
+        public int AddInt(int x, int y)
+        {
+            return x + y;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int a = p.InputInt();
+            int b = p.InputInt();
+            int suma = p.AddInt(a, b);
+            Console.WriteLine($"Suma {a} + {b} wynosi: {suma}");
+        }
+    }
+}

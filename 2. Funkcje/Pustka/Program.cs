ef5c3897849dc7a2224b9722416b789db4a3bf36
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-02-24 20:02:44 +0100

    update
    Musze zacząć używać rozwiązania w jednym folderze. Jest to lepsze rozwiązanie na GitHub'a. Dodatkowo przeszedłem na .Net 7.0 po pytaniu instruktora czy można z .Net 5.0

--- 2. Funkcje/Pustka/Program.cs
+++ 2. Funkcje/Pustka/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program p = new Program();
+            NapiszCos();
+            NapiszCos("testing");
+            NapiszCos("do zobaczenia przy klawiaturze ;)");
+            p.NapiszLOL();                                  // DON'T DO IT PLS 
+            Console.Read();
+        }
+        public void NapiszLOL() 
+        {
+            Console.WriteLine("LOL");
+        }
+        
+        public static void NapiszCos(string tekst = "domyślny tekst") 
+        {
+            Console.WriteLine($"Tekst, który miał się wyświetlić to: {tekst}");
         }
     }
 }

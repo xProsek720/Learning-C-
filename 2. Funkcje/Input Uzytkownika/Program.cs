1683d1d6377b24bc17565dc8d622e1395f3f292b
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-02-26 00:34:42 +0100

    upload
    wczoraj zrobione

---
+++ 2. Funkcje/Input Uzytkownika/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Input_Uzytkownika
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LadneDodawanie();
+            Console.Read();
+        }
+
+        public static void LadneDodawanie() 
+        {
+            int a, b;
+            Console.WriteLine("Podasz dwie liczby które zostaną dodane do siebie!");
+            Console.Write("");
+            a = int.Parse(Console.ReadLine());
+
+            //To wygląda pięknie
+            Console.CursorTop--;
+            Console.CursorLeft = a.ToString().Length;
+
+            Console.Write(" + ");
+            b = int.Parse(Console.ReadLine());
+
+            //Oj taaak byczqu <3
+            Console.CursorTop--;
+            Console.CursorLeft = a.ToString().Length + 3 + b.ToString().Length;
+
+            Console.WriteLine($" = {a + b}");
+        }
+    }
+}
---
+++ 2. Funkcje/Zadanko do Metod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Zadanko_do_Metod
+{
+    /*Metoda która przywita 3 przyjaciół bez zwracania, pobiera stringa*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PowitajPrzyjaciela("Marcel");
+            PowitajPrzyjaciela("Frank");
+            PowitajPrzyjaciela("Krystian");
+        }
+
+        private static void PowitajPrzyjaciela(string imie)
+        {
+            Console.WriteLine($"Witaj {imie}, mój przyjacielu!");
+        }
+    }
+}

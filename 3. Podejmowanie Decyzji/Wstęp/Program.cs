9fed6d02de6428ec7aacbe1172a4a10bf5b1a8eb
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-01 01:13:55 +0100

    update

--- 3. Podejmowanie Decyzji/Wstęp/Program.cs
+++ 3. Podejmowanie Decyzji/Wstęp/Program.cs
@@ -4,29 +4,40 @@
     {
         static void Main(string[] args)
         {
-            int temperatura = 15;
+            Console.Write("Podaj temperature na zewnątrz [ w C* ]: ");
+            string temperaturaInput = Console.ReadLine();
+            try
+            { 
+                int temperaturaLiczba= int.Parse(temperaturaInput);
 
-            Console.WriteLine($"Temperatura wynosi: {temperatura}");
+                Console.WriteLine($"Temperatura wynosi: {temperaturaInput}");
 
-            if (temperatura < 0) 
+                if (temperaturaLiczba < 0)
+                {
+                    Console.WriteLine("Jest mróz! Ubierz się ciepło!");
+                }
+
+                if (temperaturaLiczba > 0 && temperaturaLiczba < 10)
+                {
+                    Console.WriteLine("Weź bluzę, czapkę i długie spodnie!");
+                }
+
+                if (temperaturaLiczba > 10 && temperaturaLiczba < 20)
+                {
+                    Console.WriteLine("Weź bluzę!");
+                }
+
+                if (temperaturaLiczba > 20)
+                {
+                    Console.WriteLine("Dzisiaj jak w lato! B)");
+                }
+            }
+            catch (Exception ex) 
             {
-                Console.WriteLine("Jest mróz! Ubierz się ciepło!");
+                Console.WriteLine("Był problem z konwersją podanej temperatury! :(");
+                Console.WriteLine(ex.Message);
             }
 
-            if (temperatura > 0 && temperatura < 10) 
-            {
-                Console.WriteLine("Weź bluzę, czapkę i długie spodnie!");
-            }
-
-            if (temperatura > 10 && temperatura < 20) 
-            {
-                Console.WriteLine("Weź bluzę!");
-            }
-
-            if (temperatura > 20) 
-            {
-                Console.WriteLine("Dzisiaj jak w lato! B)");
-            }
         }
     }
 }

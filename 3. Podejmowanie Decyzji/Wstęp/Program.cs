186ecdfaa80ba568c5e43d2e7955050ca6514086
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-01 01:06:32 +0100

    upload

--- 3. Podejmowanie Decyzji/Wstęp/Program.cs
+++ 3. Podejmowanie Decyzji/Wstęp/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int temperatura = 15;
+
+            Console.WriteLine($"Temperatura wynosi: {temperatura}");
+
+            if (temperatura < 0) 
+            {
+                Console.WriteLine("Jest mróz! Ubierz się ciepło!");
+            }
+
+            if (temperatura > 0 && temperatura < 10) 
+            {
+                Console.WriteLine("Weź bluzę, czapkę i długie spodnie!");
+            }
+
+            if (temperatura > 10 && temperatura < 20) 
+            {
+                Console.WriteLine("Weź bluzę!");
+            }
+
+            if (temperatura > 20) 
+            {
+                Console.WriteLine("Dzisiaj jak w lato! B)");
+            }
         }
     }
 }

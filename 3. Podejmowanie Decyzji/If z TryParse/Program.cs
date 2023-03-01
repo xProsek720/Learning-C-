694a3b5284e3c57c4b7bd71bebd22584a1c3c5b0
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-01 22:51:58 +0100

    upload

---
+++ 3. Podejmowanie Decyzji/If z TryParse/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace If_z_TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wiekParsed;
+            Console.Write("Podaj swój wiek: ");
+            string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out wiekParsed))
+            {
+                Console.WriteLine("Udało się poprawnie wczytać wiek!");
+            }
+            else 
+            {
+                Console.WriteLine("Przykro mi, ale był problem z odczytaniem wieku!");
+            }
+
+            Console.Write($"Masz {wiekParsed}lat i możesz iść sobie kupić ");
+            if (wiekParsed >= 18)
+            {
+                Console.WriteLine("piwko.");
+            }
+            else 
+            {
+                Console.WriteLine("soczek.");
+            }
+        }
+    }
+}

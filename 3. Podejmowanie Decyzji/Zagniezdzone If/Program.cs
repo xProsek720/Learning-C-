862b0c83ce7c1676f5be30bb29f99affaf08308d
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-02 23:49:10 +0100

    upload

---
+++ 3. Podejmowanie Decyzji/Zagniezdzone If/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Zagniezdzone_If
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool jestAdminem = true;
+            bool jestZarejestrowany = true;
+
+            string nazwaUzytkownika = "xProsek";
+
+            if (jestZarejestrowany) 
+            {
+                Console.WriteLine($"Witaj {nazwaUzytkownika}!");
+                if (jestAdminem) 
+                {
+                    Console.WriteLine($"Jesteś Administratorem tego CLI!");
+                }
+            }
+            Console.Read();
+        }
+    }
+}

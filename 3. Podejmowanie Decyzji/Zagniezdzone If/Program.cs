4da451562fd9ab1a2e478ab02ba96b0515e0d334
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-02 23:55:04 +0100

    update

--- 3. Podejmowanie Decyzji/Zagniezdzone If/Program.cs
+++ 3. Podejmowanie Decyzji/Zagniezdzone If/Program.cs
@@ -4,18 +4,45 @@
     {
         static void Main(string[] args)
         {
-            bool jestAdminem = true;
-            bool jestZarejestrowany = true;
+            bool jestAdminem = false;
+            bool jestZarejestrowany = false;
 
-            string nazwaUzytkownika = "xProsek";
+            string[] admini = { "xProsek720", "m0rph3u5" };
+            string[] zarejestrowani = { "xProsek720", "m0rph3u5", "test", "gosiak2", "kontempner" };
 
-            if (jestZarejestrowany) 
+            Console.Write("Podaj nazwe użytkownika: ");
+            string nazwaUzytkownika = Console.ReadLine();
+            Console.Clear();
+
+            foreach (string s in zarejestrowani) 
+            {
+                if (s.Equals(nazwaUzytkownika)) 
+                {
+                    jestZarejestrowany = true;
+                    break;
+                }
+            }
+
+            foreach (string s in admini) 
+            {
+                if (s.Equals(nazwaUzytkownika)) 
+                {
+                    jestAdminem = true;
+                    break;
+                }
+            }
+
+            if (jestZarejestrowany)
             {
                 Console.WriteLine($"Witaj {nazwaUzytkownika}!");
-                if (jestAdminem) 
+                if (jestAdminem)
                 {
                     Console.WriteLine($"Jesteś Administratorem tego CLI!");
                 }
+            }
+            else 
+            {
+                Console.WriteLine("Przykro mi, ale nie jesteś zarejestrowanym użytkownikiem!");
             }
             Console.Read();
         }

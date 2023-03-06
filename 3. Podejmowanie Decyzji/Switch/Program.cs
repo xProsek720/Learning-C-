e87de97d33e6d3c5e7dae2205e717db87c84e85f
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-06 23:47:07 +0100

    update

--- 3. Podejmowanie Decyzji/Switch/Program.cs
+++ 3. Podejmowanie Decyzji/Switch/Program.cs
@@ -4,9 +4,53 @@
     {
         static void Main(string[] args)
         {
+
+
+            bool loggedIn = false;
+            bool canTryLuck = false;
+
+
+            Console.Write("Podaj login: ");
+            string login = Console.ReadLine();
+            switch (login) 
+            {
+                case "xProsek720":
+                    {
+                        Console.WriteLine("Czeka na ciebie losowanie!");
+                        loggedIn = true;
+                        canTryLuck = true;  
+                        break;
+                    }
+                case "root": 
+                    {
+                        Console.WriteLine("Nie możesz brać udziału w losowaniu!");
+                        loggedIn = true;    
+                        break;
+                    }
+                case "zTosieq": 
+                    {   
+                        Console.WriteLine("Czeka na ciebie losowanie!");
+                        loggedIn = true;    
+                        canTryLuck= true;
+                        break;
+                    }
+                default: 
+                    {
+                        Console.WriteLine("Nie rozpoznano użytkownika!");
+                        break;
+                    }
+            }
+
+
             int nrOpcji;
-            Console.Write("Podaj opcje(1-4): ");
-            string txtOpcja = Console.ReadLine();
+            string txtOpcja = "0";
+            if (loggedIn && canTryLuck) 
+            { 
+                Console.Write("Podaj opcje(1-4): ");
+                txtOpcja = Console.ReadLine();
+            }
+
+
             if (!int.TryParse(txtOpcja, out nrOpcji)) 
             {
                 Console.WriteLine("Nie udało się skonwertować opcji na numer! :(");
@@ -14,35 +58,42 @@
 
 
             //Oczywiście można użyć też stringa, ale chciałem użyć TryParse.
-            switch (nrOpcji) 
-            {
-                case 1: 
-                    {
-                        Console.WriteLine("Wygrałeś piwo!");
-                        break;
-                    }
-                case 2: 
-                    {
-                        Console.WriteLine("Wygrałeś nissana!");
-                        break;
-                    }
-                case 3:
-                    {
-                        Console.WriteLine("Stawiasz piwo! ;)");   
-                        break;
-                    }
-                case 4:
-                    {
-                        Console.WriteLine("Stawiasz nissana! ;)");
-                        break;
-                    }
-                default: 
-                    {
-                        Console.WriteLine("Wybrałeś błędną opcję!");
-                        break;
-                    }
+            if (canTryLuck) 
+            { 
+                switch (nrOpcji) 
+                {
+                    case 1: 
+                        {
+                            Console.WriteLine("Wygrałeś piwo!");
+                            break;
+                        }
+                    case 2: 
+                        {
+                            Console.WriteLine("Wygrałeś nissana!");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Stawiasz piwo! ;)");   
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Stawiasz nissana! ;)");
+                            break;
+                        }
+                    default: 
+                        {
+                            Console.WriteLine("Wybrałeś błędną opcję!");
+                            break;
+                        }
+                }
             }
+
+
             Console.Read();
+
+
         }
     }
 }

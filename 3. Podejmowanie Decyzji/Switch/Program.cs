ed98cc1fe0411b01d79c8d222017df41aeff9e49
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-06 23:37:15 +0100

    upload

---
+++ 3. Podejmowanie Decyzji/Switch/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nrOpcji;
+            Console.Write("Podaj opcje(1-4): ");
+            string txtOpcja = Console.ReadLine();
+            if (!int.TryParse(txtOpcja, out nrOpcji)) 
+            {
+                Console.WriteLine("Nie udało się skonwertować opcji na numer! :(");
+            }
+
+            switch (nrOpcji) 
+            {
+                case 1: 
+                    {
+                        Console.WriteLine("Wygrałeś piwo!");
+                        break;
+                    }
+                case 2: 
+                    {
+                        Console.WriteLine("Wygrałeś nissana!");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Stawiasz piwo! ;)");   
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Stawiasz nissana! ;)");
+                        break;
+                    }
+                default: 
+                    {
+                        Console.WriteLine("Wybrałeś błędną opcję!");
+                        break;
+                    }
+            }
+            Console.Read();
+        }
+    }
+}

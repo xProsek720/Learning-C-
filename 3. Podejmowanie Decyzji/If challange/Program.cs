1fdfc7a2cfc492175b472040e1e7f3c3b4b62199
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-06 23:56:40 +0100

    upload

---
+++ 3. Podejmowanie Decyzji/If challange/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace If_challange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            int highscore = 419;
+
+            string highscorePlayer = "xProsek720";
+            string playerName;
+
+            Console.Write("Give me your nickname: ");
+            playerName = Console.ReadLine();
+            Random random = new Random();
+            score = random.Next(0, 420);
+            if (score > highscore) 
+            {
+                Console.WriteLine($"You've beaten {highscorePlayer}");
+                highscore = score;
+                highscorePlayer = playerName;
+            }
+            ShowScore(score, playerName);
+            ShowHighscore(highscore, highscorePlayer);
+
+            Console.Read();
+        }
+
+        public static void ShowScore(int score, string playerName) 
+        {
+            Console.WriteLine($"Score of {playerName} from this roll is {score}");
+        }
+
+        public static void ShowHighscore(int highscore, string highscorePlayer) 
+        {
+            Console.WriteLine($"Highscore of {highscorePlayer} is {highscore}");
+        }
+
+
+    }
+}

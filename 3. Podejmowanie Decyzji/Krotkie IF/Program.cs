d9e98ad47eaa0834dcef2803af62e351c1c167e8
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-08 19:28:06 +0100

    upload

---
+++ 3. Podejmowanie Decyzji/Krotkie IF/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Krotkie_IF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //coś = warunek ? TRU : !TRU;
+            //coś = warunek ? TRU : warunek2(!TRU) ? TRU2 : !TRU2;
+
+            int temperatura = 100;
+            string stanMaterii;
+
+            stanMaterii = temperatura < 0 ? "lód" : temperatura < 100 ? "woda" : "para";
+
+            Console.WriteLine($"Gdy H20 wynosi {temperatura}*C to jej stan materii to {stanMaterii}!");
+            Console.WriteLine("[Przy zachowaniu idealnych warunków kontrolnych!]");
+            Console.Read();
+        }
+    }
+}

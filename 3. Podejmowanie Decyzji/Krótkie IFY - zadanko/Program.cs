0babc7f629a8896ff97cf11c450f09c46233867e
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-08 20:17:12 +0100

    upload

---
+++ 3. Podejmowanie Decyzji/Krótkie IFY - zadanko/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Krótkie_IFY___zadanko
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputTemperature, wiadomość, template, ifOutput;
+            int parsedTemperature;
+
+            wiadomość = "Podaj temperature: ";
+            template = "...*C";
+
+            Console.Write(wiadomość+template);
+            Console.CursorLeft = wiadomość.Length;
+            inputTemperature = Console.ReadLine();
+
+            if (!int.TryParse(inputTemperature, out parsedTemperature)) 
+            {
+                Console.WriteLine("Problem z konwersją temperatury!");
+            }
+
+            ifOutput = parsedTemperature <= 15 ? "Zimno tu, brrr." : parsedTemperature < 29 ? "Temperatura jest okej." : "Tu jest gorąco!";
+            Console.WriteLine(ifOutput);
+
+        }
+    }
+}

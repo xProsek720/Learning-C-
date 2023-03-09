a70ed8b7067e05760fc18af801305457b2463dc9
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-09 23:02:28 +0100

    upload

---
+++ 3. Podejmowanie Decyzji/Pętla For/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Pętla_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pierwsza pętla For:");
+            //  wart. start     warunek    zwiekszanie
+            for (int licznik = 0; licznik < 10; licznik++)
+            {
+                Console.WriteLine($"{licznik + 1} jest mniejsze od 10");
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("\nDruga pętla For:");
+            // można zwiększać np. o 5
+            for (int licznik = 0; licznik < 50; licznik+=5)
+            {
+                Console.WriteLine($"{licznik + 1} jest mniejsze od 50");
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("\nTrzecia pętla For:");
+            for (int licznik = 1; licznik < 21; licznik += 2) 
+            {
+                Console.WriteLine($"{licznik} jest liczbą nieparzystą mniejszą od 20");
+            }
+            Console.ReadKey();
+        }
+    }
+}

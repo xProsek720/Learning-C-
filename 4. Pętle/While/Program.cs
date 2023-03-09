9e959488533a97270a480228ca1c269f7dbad087
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-09 23:32:58 +0100

    upload

---
+++ 4. Pętle/While/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml;
+
+namespace While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int licznik = 0;
+
+            Console.WriteLine("Pierwszy While: ");
+
+            while (licznik < 10) 
+            {
+                Console.WriteLine($"{licznik} jest mniejsze niż 10");
+                licznik++;
+            }
+
+
+            Console.WriteLine("\nDrugi While: ");
+
+            int licznikLudzi = 0;
+            string text = $"Jest Ludzi: ";
+            Console.WriteLine(text + "0");
+            Console.WriteLine("Kliknij ENTER aby policzyć osobę!");
+            Console.CursorTop-=2;
+            while (licznikLudzi < 30) 
+            {
+
+                if (Console.Read() == 13) 
+                {
+                    Console.CursorTop--;
+                    Console.CursorLeft = text.Length;
+                    Console.WriteLine(licznikLudzi++ + 1);
+                    Console.CursorTop--;
+                }
+            }
+            Console.WriteLine("Wszyscy są!");
+
+            Console.ReadKey();
+        }
+    }
+}

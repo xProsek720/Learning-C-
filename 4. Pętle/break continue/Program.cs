9eff5583386341e95f5af517cb7ec79be393433d
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-16 22:32:39 +0100

    upload

---
+++ 4. Pętle/break continue/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace break_continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++) 
+            {
+                Console.WriteLine(i);
+                if (i == 6) 
+                {
+                    Console.WriteLine("Zatrzymujemy się na 6.");
+                    break;
+                }
+            }
+            Console.Read();
+
+            for (int i = 0; i <= 12; i++)
+            {
+                if (i % 2 == 0) 
+                {
+                    Console.WriteLine($"We skip {i}.");
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}

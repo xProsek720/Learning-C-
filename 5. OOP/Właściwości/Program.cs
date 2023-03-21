b43ca267e6183ce1b812cf091000eec47a505a55
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-22 00:29:47 +0100

    upload

---
+++ 5. OOP/Właściwości/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Właściwości
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            int counter = 1;
+            Pudelko[] arr = new Pudelko[4];
+            Pudelko p1 = new Pudelko();
+            arr[0] = p1;
+            Pudelko p2 = new Pudelko(1);
+            arr[1] = p2;
+            Pudelko p3 = new Pudelko(1,2);
+            arr[2] = p3;
+            Pudelko p4 = new Pudelko(1,2,3);
+            arr[3] = p4;
+            foreach( Pudelko p in arr ) 
+            {
+                Console.WriteLine( $"Pudełko nr {counter++}: ");
+                PrintPudelko(p);
+                Console.WriteLine();
+            }
+            
+        }
+        public static void PrintPudelko(Pudelko p) 
+        {
+            Console.WriteLine($"    Długość: {p.ZwrocDlugosc()}");
+            Console.WriteLine($"    Szerokość: {p.ZwrocSzerokosc()}");
+            Console.WriteLine($"    Wysokość: {p.ZwrocWysokosc()}");
+            Console.WriteLine($"    Objętość: {p.ZwrocObjetosc()}");
+        }
+    }
+}
---
+++ 5. OOP/Właściwości/Pudelko.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Właściwości
+{
+    internal class Pudelko
+    {
+        //member variable
+        //to akurat po angielsku
+        private int dlugosc = -1;
+        private int szerokosc = -1;
+        private int wysokosc;
+        private int objetosc;
+
+        public Pudelko()
+        { 
+        }
+        public Pudelko(int dlugosc) 
+        {
+            this.dlugosc = dlugosc;
+        }
+        public Pudelko(int dlugosc, int szerokosc) 
+        {
+            this.dlugosc = dlugosc;
+            this.szerokosc = szerokosc;
+        }
+        public Pudelko(int dlugosc, int szerokosc, int wysokosc)
+        {
+            this.dlugosc = dlugosc;
+            this.szerokosc = szerokosc;
+            this.wysokosc = wysokosc;
+            PoliczObjetosc();
+        }
+
+        public void UstawDlugosc(int dlugosc) 
+        {
+            this.dlugosc = dlugosc;
+        }
+        public void UstawSzerokosc(int szerokosc) 
+        {
+            this.szerokosc = szerokosc;
+        }
+        public void UstawWysokosc(int wysokosc) 
+        {
+            this.wysokosc = wysokosc;
+        }
+        public int ZwrocDlugosc() 
+        {
+            return this.dlugosc;
+        }
+        public int ZwrocSzerokosc() 
+        {
+            return (this.szerokosc);
+        }
+        public int ZwrocWysokosc() 
+        {
+            return (this.wysokosc);
+        }
+        public int ZwrocObjetosc() 
+        {
+            if (objetosc > 0)
+            {
+                return (this.objetosc);
+            }
+            else { return 0; }
+        }
+        protected void PoliczObjetosc() 
+        {
+            if (wysokosc > 0 && szerokosc > 0 && dlugosc > 0) 
+            {
+                this.objetosc = wysokosc * szerokosc * dlugosc;
+            }
+        }
+
+    }
+}

ac3c2f3570a524d3c51a3e21fde289b506c51c01
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-23 23:17:55 +0100

    upload

---
+++ 5. OOP/Member i Destruktory/Czlonek.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Member_i_Destruktory
+{
+    internal class Czlonek
+    {
+
+        private string _nazwaCzlonka;
+        private string _nazwaStanowiska;
+        private int _wyplata;
+
+        public int wiek;
+
+        public string NazwaStanowiska
+        {
+            get
+            {
+                return _nazwaStanowiska;
+            }
+            set
+            {
+                _nazwaStanowiska = value;
+            }
+        }
+        public Czlonek() 
+        {
+        
+        }
+        public Czlonek(string _nazwaCzlonka, int wiek) 
+        {
+            this._nazwaCzlonka= _nazwaCzlonka;
+            this.wiek = wiek;
+        }
+        public Czlonek(string _nazwaCzlonka, int wiek, string _nazwaStanowiska, int _wyplata) 
+        {
+            this._nazwaCzlonka = _nazwaCzlonka;
+            this.wiek = wiek;
+            this._nazwaStanowiska = _nazwaStanowiska;
+            this._wyplata = _wyplata;
+        }
+        ~Czlonek() 
+        {
+            Console.WriteLine("Obiekt unicestwiony!");
+        }
+        public void PrzedstawSie(bool isFriend) 
+        {
+            if (_nazwaCzlonka != null) 
+            {
+                Console.WriteLine($"Nazywam się {_nazwaCzlonka}.");
+            }
+            if (_nazwaStanowiska != null) 
+            {
+                Console.WriteLine($"Pracuje jako {_nazwaStanowiska}.");
+            }
+            if (wiek != 0) 
+            {
+                Console.WriteLine($"Mam {wiek}lat.");
+            }
+            if (isFriend) 
+            {
+                PodzielSiePrywatnymiInformacjami();
+            }
+            Console.WriteLine();
+        }
+        private void PodzielSiePrywatnymiInformacjami() 
+        {
+            if (_wyplata > 0) 
+            {
+                Console.WriteLine($"Zarabiam {_wyplata}!");
+            }
+        }
+
+    }
+}
---
+++ 5. OOP/Member i Destruktory/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Member_i_Destruktory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+
+            Czlonek nieznany = new Czlonek();
+            Czlonek mL = new Czlonek("Maryla", 99);
+            Czlonek kL = new Czlonek("Karolina", 20, "Sekretarka", 10000);
+            
+            Console.WriteLine($"Osoba {counter++}:");
+            nieznany.NazwaStanowiska = "Sprzątacz";
+            nieznany.PrzedstawSie(false);
+
+            Console.WriteLine($"Osoba {counter++}:");
+            mL.PrzedstawSie(false);
+
+            Console.WriteLine($"Osoba {counter++}:");
+            kL.PrzedstawSie(true);
+        }
+    }
+}

9998e5b0ff9dd01fa73add50c484258e6a5beddf
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-24 21:12:44 +0100

    upload

---
+++ 6. Zbiory/Tablica/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Tablica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] oceny = new int[5];
+
+            oceny[0] = 4;
+            oceny[1] = 4;
+            oceny[2] = 2;
+            oceny[3] = 5;
+            oceny[4] = 3;
+
+            int index = 0;
+            Console.WriteLine($"Ocena na indexie {index}: {oceny[index]}");
+
+            Console.Write("Podaj nową ocene: ");
+            string input = Console.ReadLine();
+            int.TryParse( input, out oceny[0]);
+            
+            Console.WriteLine($"Ocena na indexie {index}: {oceny[index]}");
+            Console.Read();
+
+            int[] ocenyZMatmy = { 4, 4, 3, 5, 5, };
+
+            int[] ocenyZPolskiego = new int[] { 3, 2, 5, 6, 4, 5};
+
+            Console.WriteLine($"Uczen ma {ocenyZMatmy.Length} ocen z matematyki.");
+            Console.WriteLine($"Uczen ma {ocenyZPolskiego.Length} ocen z j. polskiego.");
+        }
+    }
+}

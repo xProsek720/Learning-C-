15a33f9fa6a2019be7b42ade040e0a139734ebdb
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-04-06 00:59:41 +0200

    upload

---
+++ 6. Zbiory/Postrzepione Tablice - challange/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Postrzepione_Tablice___challange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rodzinaMarcela = new string[] { "Magda", "Mietek"};
+
+            string[][] kumpleIRodzinyy = new string[][] 
+            {
+                new string[] {"Zbigniew", "Klaudia"},
+                new string[] {"Julia", "Dorota"},
+                new string[] {"Kamila", "Filip"},
+                rodzinaMarcela
+            };
+
+            foreach (string[] kumpel in kumpleIRodzinyy) 
+            {
+                Console.WriteLine($"Cześć {kumpel[0]}, poznaj {kumpel[1]}!");
+            }
+            Console.Read();
+        }
+    }
+}

6478f9af4185ab43047eec288d179a6faae40754
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-04-06 01:15:56 +0200

    update
    there was challange in class

--- 6. Zbiory/Uzywanie Tablicy jako Parametr/Program.cs
+++ 6. Zbiory/Uzywanie Tablicy jako Parametr/Program.cs
@@ -8,6 +8,19 @@
 
             WyswietlOceny(oceny);
             Console.WriteLine($"Średnia tego ucznia wynosi: {Srednia(oceny)}");
+
+            Console.WriteLine("\n");
+
+            //Challange
+            int[] szczescie = new int[] { 3, 3, 2, 3, 3 };
+            PokazSzczescie(szczescie);
+            ZwiekszSzczescie(szczescie);
+            PokazSzczescie(szczescie);
+            ZwiekszSzczescie(szczescie);
+            PokazSzczescie(szczescie);
+            Console.ReadLine();
+
+
         }
         public static void WyswietlOceny(int[] oceny) 
         {
@@ -30,6 +43,26 @@
             }
             srednia = (double) sum / (double) dlugosc;
             return srednia;
+        }
+
+        public static int[] ZwiekszSzczescie(int[] szczescie) 
+        {
+            Console.WriteLine("Zwiekszam poziomy szczęścia o 2");
+            for (int i =0; i < szczescie.Length; i++) 
+            {
+                szczescie[i] += 2;
+            }
+            return szczescie;
+        }
+
+        public static void PokazSzczescie(int[] szczescie) 
+        {
+            Console.Write("Poziomy szczescia tego ucznia: ");
+            foreach (int poziom in szczescie)
+            {
+                Console.Write($"{poziom} ");
+            }
+            Console.WriteLine();
         }
     }
 }

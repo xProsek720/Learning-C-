7703888cf1608235d5747c361026e2e21aa2ec32
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-04-06 01:09:01 +0200

    upload

---
+++ 6. Zbiory/Uzywanie Tablicy jako Parametr/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Uzywanie_Tablicy_jako_Parametr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] oceny = new int[] { 5, 5, 4, 4};
+
+            WyswietlOceny(oceny);
+            Console.WriteLine($"Średnia tego ucznia wynosi: {Srednia(oceny)}");
+        }
+        public static void WyswietlOceny(int[] oceny) 
+        {
+            Console.Write("Oceny tego ucznia: ");
+            foreach (int ocena in oceny)
+            {
+                Console.Write($"{ocena} ");
+            }
+            Console.WriteLine();
+        }
+        public static double Srednia(int[] oceny) 
+        {
+            int dlugosc = oceny.Length;
+            int sum = 0;
+            double srednia;
+
+            foreach (int i in oceny) 
+            {
+                sum += i;
+            }
+            srednia = (double) sum / (double) dlugosc;
+            return srednia;
+        }
+    }
+}

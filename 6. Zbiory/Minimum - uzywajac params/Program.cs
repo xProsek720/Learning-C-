866ca0d633c54ec678ffd2814dce2a806d00fdcc
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-04-08 23:22:57 +0200

    template

---
+++ 6. Zbiory/Minimum - uzywajac params/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Minimum___uzywajac_params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

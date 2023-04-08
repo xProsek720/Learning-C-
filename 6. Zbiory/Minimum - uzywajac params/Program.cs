792678297aecfce6e28f967d2de4e79075f8dce0
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-04-08 23:45:25 +0200

    upload

--- 6. Zbiory/Minimum - uzywajac params/Program.cs
+++ 6. Zbiory/Minimum - uzywajac params/Program.cs
@@ -4,7 +4,81 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Lista: {(-2, 14, 13, 12, 1, 2)}");
+            Console.WriteLine($"Minimum z tej listy: {Minimum(-2,14,13,12,1,2)}");
+            Console.WriteLine($"Lista: {(15, 14, 13, 12, 2, 1)}");
+            Console.WriteLine($"Minimum z tej listy: {Minimum(15, 14, 13, 12, 2, 1)}");
+            Console.WriteLine($"Lista: {(1)}");
+            Console.WriteLine($"Minimum z tej listy: {Minimum(1)}");
+
+            Console.WriteLine($"Lista: {(1.2, 1.4, 1.0923982389, 0.5784355438, 0.521652)}");
+            Console.WriteLine($"Minimum z tej listy: {Minimum(1.2, 1.4, 1.0923982389, 0.5784355438, 0.521652)}");
+
+            Console.ReadLine();
         }
+
+
+        public static int Minimum(params int[] args) 
+        {
+            int min = args[0];
+            if (args.Length == 0) min = int.MaxValue;
+            for (int i = 1; i < args.Length; i++) 
+            {
+                if (args[i] < min) min = args[i];
+            }
+            return min;
+        }
+        public static double Minimum(params double[] args)
+        {
+            double min = args[0];
+            if (args.Length == 0) min = double.MaxValue;
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (args[i] < min) min = args[i];
+            }
+            return min;
+        }
+        public static float Minimum(params float[] args)
+        {
+            float min = args[0];
+            if (args.Length == 0) min = float.MaxValue;
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (args[i] < min) min = args[i];
+            }
+            return min;
+        }
+        public static long Minimum(params long[] args)
+        {
+            long min = args[0];
+            if (args.Length == 0) min = long.MaxValue;
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (args[i] < min) min = args[i];
+            }
+            return min;
+        }
+        public static short Minimum(params short[] args)
+        {
+            short min = args[0];
+            if (args.Length == 0) min = short.MaxValue;
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (args[i] < min) min = args[i];
+            }
+            return min;
+        }
+        public static sbyte Minimum(params sbyte[] args)
+        {
+            sbyte min = args[0];
+            if (args.Length == 0) min = sbyte.MaxValue;
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (args[i] < min) min = args[i];
+            }
+            return min;
+        }
+        //I tak dalej...
+        //Starczy haha
     }
 }

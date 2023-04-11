04ee217061b83554fa1729ec59358ee331ac2fef
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-04-11 23:36:40 +0200

    upload

---
+++ 6. Zbiory/HashTable - Zadanie/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+namespace HashTable___Zadanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] listaStudenow = new Student[]
+            {
+                new Student(1, "Kamil", 3.92f),
+                new Student(2, "Natalia", 2.92f),
+                new Student(3, "Leon", 4.92f),
+                new Student(4, "Filip", 4.20f)
+            };
+
+            Hashtable studenci = new Hashtable();
+            studenci.Add(1, new Student(1, "Marcin", 4.61f));
+
+            foreach (Student student in listaStudenow) 
+            {
+                if (!studenci.ContainsKey(student.Id))
+                {
+                    studenci.Add(student.Id, student);
+                }
+                else 
+                {
+                    Console.WriteLine($"Student z id: {student.Id} już istnieje!");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pora zobaczyć jacy studenci się załadowali: ");
+
+            foreach (Student student in studenci.Values) 
+            {
+                Console.WriteLine($"ID: {student.Id} | Imie {student.Name} | GPA: {student.GPA}");
+            }
+            Console.ReadLine();
+
+            
+        }
+    }
+}
---
+++ 6. Zbiory/HashTable - Zadanie/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable___Zadanie
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float GPA { get; set; }
+
+        public Student(int id, string name, float GPA) 
+        {
+            this.Id = id;
+            this.Name = name;
+            this. GPA = GPA;
+        }
+    }
+
+}

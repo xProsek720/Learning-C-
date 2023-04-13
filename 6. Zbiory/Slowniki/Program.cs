71acc3a5f0da28bfce8cd8cdf56879af9166da8d
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-04-13 23:23:59 +0200

    upload

---
+++ 6. Zbiory/Slowniki/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Slowniki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<int, string> slownik1 = new Dictionary<int, string>() 
+            {
+                {1, "one"},
+                {2, "two"},
+                {3, "three" }
+            };
+
+            Dictionary<string, string> kody = new Dictionary<string, string>();
+            kody.Add("KTW", "Katowice");
+            kody["BCN"] = "Barcelona";
+                        
+            for (int i = 1; i <= slownik1.Count; i++) 
+            {
+                Console.WriteLine($"slownik1[{i}] = { slownik1[i]}");
+            }
+            
+            Console.WriteLine();
+            foreach (string klucz in kody.Keys) 
+            {
+                Console.WriteLine($"{klucz}: {kody[klucz]}");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}

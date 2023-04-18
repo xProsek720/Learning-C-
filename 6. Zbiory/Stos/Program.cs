fe89e3dc40d379548438a2fae05b8a5f7bd0108c
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-04-18 23:36:34 +0200

    update
    added two methods one for printing all stack values line by line and one for reversing int array, enjoy

--- 6. Zbiory/Stos/Program.cs
+++ 6. Zbiory/Stos/Program.cs
@@ -18,6 +18,12 @@ namespace Stos
             Console.WriteLine($"Na górze stosu znajduje się: {stos.Peek()}");
             stos.Push(3);
             Console.WriteLine($"Na górze stosu znajduje się: {stos.Peek()}");
+            stos.Push(4);
+            Console.WriteLine($"Na górze stosu znajduje się: {stos.Peek()}");
+            stos.Push(5);
+            Console.WriteLine($"Na górze stosu znajduje się: {stos.Peek()}");
+            stos.Push(6);
+            Console.WriteLine($"Na górze stosu znajduje się: {stos.Peek()}");
 
             //Wyciągnięcie wartości ze stosu (usuwa ją)
             //Ze sprawdzeniem czy na pewno coś jest na stosie
@@ -25,7 +31,37 @@ namespace Stos
             if (stos.Count > 0)
                 popInt = stos.Pop();
             Console.WriteLine($"Wyciągnięto ze stosu: {popInt}");
-        
+
+            Console.WriteLine();
+            Console.WriteLine("Test wypisującej stos Int: ");
+            WyciagnijOrazWypisz(stos);
+
+            Console.WriteLine();
+            Console.WriteLine("Test metody odwracającej tablice Int: ");
+            OdwrocTabliceInt((new int[] { 1, 2, 3, 4, 5, 6 }));
+
+            Console.ReadLine();
         }
+
+        public static void WyciagnijOrazWypisz(Stack<int> stos) 
+        {
+            int counter = 1;
+            while (stos.Count > 0) 
+            {
+                Console.WriteLine($"{counter++}. {stos.Pop()}");
+            }
+            Console.WriteLine("Stos jest pusty!");
+        }
+
+        public static void OdwrocTabliceInt(int[] ints) 
+        {
+            Stack<int> revers = new Stack<int>();
+            foreach (int liczba in ints) 
+            {
+                revers.Push(liczba);
+            }
+            WyciagnijOrazWypisz(revers);
+        }
+
     }
 }

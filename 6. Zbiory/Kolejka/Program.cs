c03f1a552b2a4f2e0c55027113fb4cddadd6331d
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-04-19 23:22:09 +0200

    upload

---
+++ 6. Zbiory/Kolejka/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Kolejka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int helper;
+            Queue<int> kolejka = new Queue<int>();
+
+            if (kolejka.TryPeek(out helper)) 
+            {
+                Console.WriteLine(helper);
+            }
+            //Dodawanie wartości do kolejki
+            kolejka.Enqueue(1);
+            kolejka.Enqueue(2);
+            kolejka.Enqueue(3);
+            kolejka.Enqueue(4);
+            kolejka.Enqueue(5);
+            kolejka.Enqueue(6);
+
+            //Pewne Podglądanie wartości z kolejki
+            Console.WriteLine($"Nastepny nr z kolejki to: {kolejka.Peek()}");
+
+            //Wyciąganie wartości z kolejki
+            //Warto dodać ifa
+            while (kolejka.Count > 1) 
+            {
+                Console.WriteLine($"Proszony nr z kolejki: {kolejka.Dequeue()}");
+                Console.WriteLine($"Nastepny nr z kolejki to: {kolejka.Peek()}");
+            }
+            Console.WriteLine($"Proszony nr z kolejki: {kolejka.Dequeue()}");
+
+        }
+    }
+}

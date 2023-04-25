b0b6606b2c8d25e373403d6d9726dbedbaeaad37
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-04-25 23:46:44 +0200

    upload

--- 6. Zbiory/Kolejka/Program.cs
+++ 6. Zbiory/Kolejka/Program.cs
@@ -30,7 +30,51 @@
                 Console.WriteLine($"Nastepny nr z kolejki to: {kolejka.Peek()}");
             }
             Console.WriteLine($"Proszony nr z kolejki: {kolejka.Dequeue()}");
+            Console.WriteLine();
 
+
+
+            Queue<Zamowienie> kolejkaZamowien = new Queue<Zamowienie>();
+            ForEachKolejka(kolejkaZamowien, JedenCiagZamowien());
+            ForEachKolejka(kolejkaZamowien, DrugiCiagZamowien());
+
+            while (kolejkaZamowien.Count > 0) 
+            {
+                Zamowienie biezaceZamowienie = kolejkaZamowien.Dequeue();
+                biezaceZamowienie.PrzetworzZamowienie();
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+
+        public static void ForEachKolejka(Queue<Zamowienie> kolejkaZamowien, Zamowienie[] funkcja) 
+        {
+            foreach (Zamowienie z in funkcja)
+            {
+                kolejkaZamowien.Enqueue(z);
+            }
+        }
+
+        public static Zamowienie[] JedenCiagZamowien()
+        {
+            Zamowienie[] zamowienia = new Zamowienie[]
+            {
+                new Zamowienie(3, 2, 420),
+                new Zamowienie(4, 1, 77),
+                new Zamowienie(5, 1, 500)
+            };
+            return zamowienia;
+        }
+        public static Zamowienie[] DrugiCiagZamowien()
+        {
+            Zamowienie[] zamowienia = new Zamowienie[]
+            {
+                new Zamowienie(0, 5, 420),
+                new Zamowienie(1, 5, 77),
+                new Zamowienie(2, 3, 510)
+            };
+            return zamowienia;
         }
     }
 }
---
+++ 6. Zbiory/Kolejka/Zamowienie.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolejka
+{
+    internal class Zamowienie
+    {
+        public int OrderID {get; set;}
+        public int OrderQuantity { get; set;}
+        public int ProductID { get; set;}
+
+        public Zamowienie()
+        {
+        }
+        public Zamowienie(int OrderID)
+        {
+            this.OrderID = OrderID;
+        }
+        public Zamowienie(int OrderID, int OrderQuantity)
+        {
+            this.OrderID = OrderID;
+            this.OrderQuantity = OrderQuantity;
+        }
+        public Zamowienie(int OrderID, int OrderQuantity, int ProductID)
+        {
+            this.OrderID = OrderID;
+            this.OrderQuantity = OrderQuantity;
+            this.ProductID = ProductID;
+        }
+        public void PrzetworzZamowienie() 
+        {
+            Console.WriteLine($"Zamówiono {OrderQuantity}x {ProductID}");
+            Console.WriteLine($"Przetworzono Zamówienie #{OrderID}!");
+        }
+    }
+}

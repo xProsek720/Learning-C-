848fb195aaab3445a6db9fbbe0bbb94d199a490b
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-06-11 21:29:39 +0200

    upload

---
+++ 13.WPF/Data Binding/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Data_Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //ONE TIME 
+            Slajder3.Value= 23;
+            TextBox1.Text = Slajder3.Value.ToString();
+        }
+    }
+}

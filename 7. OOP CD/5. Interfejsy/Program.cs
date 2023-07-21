ea0a86ea863c3fcf495dc01c6cbc6456c2549360
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-07-21 23:54:11 +0200

    upload

--- 7. OOP CD/5. Interfejsy/Program.cs
+++ 7. OOP CD/5. Interfejsy/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Ticket t1 = new Ticket(14);
+            Ticket t2 = new Ticket(14);
+
+            Console.WriteLine(t2.Equals(t1));
+
+            t1.DurationInHours = 15;
+            Console.WriteLine(t1.Equals(t2));
         }
     }
 }
---
+++ 7. OOP CD/5. Interfejsy/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._Interfejsy
+{
+    internal class Ticket : IEquatable<Ticket>
+    {
+
+        public int DurationInHours { get; set; }
+
+        public Ticket(int DurationInHours) 
+        {
+            this.DurationInHours = DurationInHours;
+        }
+
+
+        //Pozwala nam porównywać dwa obiekty typu Ticket, czy sa rowne
+        // porównuje je ze względu na zmienną "DurationInHours".
+        public bool Equals(Ticket other) 
+        {
+            return this.DurationInHours == other.DurationInHours;
+        }
+
+    }
+}

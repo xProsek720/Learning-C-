771cdf0d7ea61540bac7aa41250ce88ac3b89aae
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-07-21 23:49:38 +0200

    blank

---
+++ 7. OOP CD/5. Interfejsy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _5._Interfejsy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

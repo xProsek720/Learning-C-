25f9709f7296a659d1c3c6614b7a6dced4bfc450
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-07-23 23:41:14 +0200

    blank
    Pardon me blanking, I've got holidayz B)

---
+++ 7. OOP CD/6. Tworzenie Interfejsow/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _6._Tworzenie_Interfejsow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

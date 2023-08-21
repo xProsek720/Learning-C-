efe4c6e7755bca9d7c55828b2b893a79165da44d
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-08-21 02:32:18 +0200

    prepload
    :P

---
+++ 7. OOP CD/7. IEnumerable - wstep/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _7._IEnumerable___wstep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
